e4fb766dab94bb756cbe042a42a700339049eb0e
duyinqiang <duyinqiang@hualala.com>
2018-08-23 13:32:59 +0800

    修改query

--- src/generator/GeneratorMain.java
+++ src/generator/GeneratorMain.java
@@ -1,7 +1,7 @@
 package generator;
 public class GeneratorMain {
 	public static void main(String[] args) throws Exception {
-		String[] tables={"tbl_mes_work_station","tbl_mes_throw_material_site","tbl_mes_procedure"};
+		String[] tables={"tbl_mes_procedure_goods"};
 		OutFiel.outFiel1(tables);
 	}
 	
--- template-hualala/java/query/${className}Query.java
+++ template-hualala/java/query/${className}Query.java
@@ -3,61 +3,26 @@
 package ${package_query};
 
 import com.alibaba.fastjson.JSON;
-
+import lombok.Data;
 import ${package_base_query};
 
 <#list table.columns as column>
 <#if column.jdbcType=='DECIMAL'>
-import java.math.BigDecimal;
 </#if>
 </#list>
 
 <#include "/java_hualala_imports.include">
+@Data
 public class ${className}Query extends QueryParam {
     private static final long serialVersionUID = 5454155825314635342L;
 
 	<#list table.columns as column>
-	/** ${column.columnAlias!}  db_column: ${column.sqlName} */	
+	/** ${column.columnAlias!}  db_column: ${column.sqlName} */
 	private ${column.javaType} ${column.columnNameLower}<#if column.columnNameLower=='isDeleted'>=0</#if>;
 	</#list>
-    
-	public ${className}Query(){
-	}
-	public ${className}Query(Long id){
-	    this.id = id;
-	}
-	<#list table.columns as column>
-	<#if column.sqlName=='code'>
-	public ${className}Query(String code){
-	    this.code = code;
-	}
-	</#if>
-	</#list>
-	
-	<@generateJavaColumns/>
+
 	@Override
 	public String toString() {
 		return JSON.toJSONString(this);
 	}
 }
-
-<#macro generateJavaColumns>
-	<#list table.columns as column>
-		<#if column.isDateTimeColumn>
-	public String get${column.columnName}String() {
-		return DateConvertUtils.format(get${column.columnName}(), FORMAT_${column.constantName});
-	}
-	public void set${column.columnName}String(String value) {
-		set${column.columnName}(DateConvertUtils.parse(value, FORMAT_${column.constantName},${column.javaType}.class));
-	}
-	
-		</#if>	
-	public ${column.javaType} get${column.columnName}() {
-		return this.${column.columnNameLower};
-	}
-	public void set${column.columnName}(${column.javaType} value) {
-		this.${column.columnNameLower} = value;
-	}
-	
-	</#list>
-</#macro>
